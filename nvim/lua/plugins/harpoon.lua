be0dd36ee1ccbf52c1780750166d8ddeccca2458
lukesarrazine <92548188+lukesarrazine@users.noreply.github.com>
2025-02-23 11:49:18 -0500

    Fixing some features with adding harpoon marks

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -11,23 +11,37 @@ return {
 
         harpoon.setup()
         map("n", "<leader>a", function() harpoon:list():add() end, {})
-        map("n", "<leader>d", function() harpoon:list():delete() end, {})
 
         -- Telescope UI
         local conf = require("telescope.config").values
         local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-                table.insert(file_paths, item.value)
+            local finder = function()
+                local paths = {}
+                for _, item in ipairs(harpoon_files.items) do
+                    table.insert(paths, item.value)
+                end
+
+                return require("telescope.finders").new_table({
+                    results = paths,
+                })
             end
 
             require("telescope.pickers").new({}, {
                 prompt_title = "Harpoon",
-                finder = require("telescope.finders").new_table({
-                    results = file_paths,
-                }),
+                finder = finder(),
                 previewer = conf.file_previewer({}),
                 sorter = conf.generic_sorter({}),
+                attach_mappings = function(prompt_bufnr, map_harpoon)
+                    map_harpoon("i", "<C-a>", function()
+                        local state = require("telescope.actions.state")
+                        local selected_entry = state.get_selected_entry()
+                        local current_picker = state.get_current_picker(prompt_bufnr)
+
+                        table.remove(harpoon_files.items, selected_entry.index)
+                        current_picker:refresh(finder(), { reset_prompt = false })
+                    end)
+                    return true
+                end,
             }):find()
         end
 
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,12 +2,15 @@ return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
+        "ThePrimeagen/harpoon",
     },
     config = function()
         require("ext-dependencies.ripgrep").ensure_ripgrep()
 
         local telescope = require("telescope")
+        local conf = require("telescope.config")
         local builtin = require("telescope.builtin")
+        local harpoon = require("harpoon")
 
         telescope.setup({
             defaults = {
@@ -16,6 +19,12 @@ return {
                         ["<C-j>"] = "move_selection_next",
                         ["<C-k>"] = "move_selection_previous",
                     },
+                },
+                layout_config = {
+                    height = 0.45,
+                    width = 0.7,
+                    prompt_position = "bottom",
+                    preview_cutoff = 120,
                 },
                 file_ignore_patterns = {
                     "target/",
@@ -27,8 +36,59 @@ return {
             },
         })
 
-        -- Key mappings
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+        local function find_files_with_harpoon()
+            local function get_finder()
+                local harpoon_list = harpoon:list()
+                local harpoon_set = {}
+
+                for _, item in ipairs(harpoon_list.items) do
+                    harpoon_set[item.value] = true
+                end
+
+                return require("telescope.finders").new_table({
+                    results = vim.fn.systemlist("rg --files"), --use ripgrep
+                    entry_maker = function(entry)
+                        local filename = vim.fn.fnamemodify(entry, ":p") -- Get absolute path
+                        local icon = harpoon_set[entry] and "⭐ " or "  " -- Harpoon marker
+
+                        return {
+                            value = entry,
+                            display = icon .. entry, -- Add icon dynamically
+                            ordinal = entry,
+                            path = filename,
+                        }
+                    end
+                })
+            end
+
+            -- Invoke Telescope with the custom finder
+            require("telescope.pickers").new({}, {
+                prompt_title = "Find Files",
+                finder = get_finder(),
+                sorter = require("telescope.config").values.generic_sorter({}),
+                attach_mappings = function(prompt_bufnr, map)
+                    map("i", "<C-a>", function()
+                        local state = require("telescope.actions.state")
+                        local entry = state.get_selected_entry()
+                        local current_picker = state.get_current_picker(prompt_bufnr)
+
+                        if entry and entry.value then
+                            harpoon:list():add({ value = entry.value, context = { col = 0, row = 1 } })
+                            vim.notify("Added to Harpoon: " .. entry.value, vim.log.levels.INFO)
+
+                            entry.display = entry.display .. "⭐ "
+                            current_picker:refresh(current_picker.finder, { reset_prompt = false })
+                        else
+                            vim.notify("Invalid file selection", vim.log.levels.ERROR)
+                        end
+                    end)
+                    return true
+                end,
+            }):find()
+        end
+
+        -- Keymaps
+        vim.keymap.set("n", "<leader>ff", find_files_with_harpoon, { desc = "Find Files (Harpoon Highlighted)" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
         vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help Tags" })
